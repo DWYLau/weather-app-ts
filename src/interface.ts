1ab836d86220fab3a1cbc9fba8b1abb6da28794b
Derek <dereklau15@gmail.com>
2023-10-18 22:01:38 +0900

    Add change information functions

--- src/interface.ts
+++ src/interface.ts
@@ -1,33 +1,83 @@
 import * as Weather from "./weather";
-export * as Interface from "./interface";
 
 const location = document.getElementById("location") as HTMLInputElement | null;
-const search = document.getElementById("search") as HTMLElement;
-const city = document.getElementById("city");
-const date = document.getElementById("date");
-const countryName = document.getElementById("country");
-const temperature = document.getElementById("temp");
-const feelsLike = document.getElementById("feels-like");
-const precipitation = document.getElementById("precipitation");
-const humidity = document.getElementById("humidity");
-const windSpeed = document.getElementById("wind-speed");
-const gustSpeed = document.getElementById("gust-speed");
-const description = document.getElementById("description");
-const weatherImage = document.getElementById("weather-image");
-const weatherCardDiv = document.querySelector(".weather-cards");
-const error = document.querySelector(".error");
+const temperature = document.getElementById("temp") as HTMLElement;
+const feelsLike = document.getElementById("feels-like") as HTMLElement;
+const precipitation = document.getElementById("precipitation") as HTMLElement;
+const humidity = document.getElementById("humidity") as HTMLElement;
+const windSpeed = document.getElementById("wind-speed") as HTMLElement;
+const gustSpeed = document.getElementById("gust-speed") as HTMLElement;
+const description = document.getElementById("description") as HTMLElement;
+const error = document.getElementById("error") as HTMLElement;
+const weatherImage = document.getElementById(
+  "weather-image"
+) as HTMLImageElement;
+const weatherCardDiv = document.querySelector(
+  ".weather-cards"
+) as HTMLDivElement;
 
-const API_KEY = "226dbf12f6c9f3e021556ea66e7c95c9";
+export function changeInfo(cityName: string, countryName: string): void {
+  const city = document.getElementById("city") as HTMLElement;
+  const country = document.getElementById("country") as HTMLElement;
+  city.textContent = cityName;
+  country.textContent = countryName;
+  if (city.textContent.length >= 20) {
+    city.style.fontSize = "2rem";
+  }
+}
+
+export function changeDate(dateData: string) {
+  const date = document.getElementById("date") as HTMLElement;
+  const options = {
+    weekday: "long",
+    month: "long",
+    day: "numeric",
+    year: "numeric",
+  } as const;
+  date.textContent = new Date(dateData).toLocaleDateString("en-GB", options);
+}
+
+export function changeError(): void {
+  location.style.marginTop = "2.1rem";
+  error.style.display = "block";
+  error.textContent = "Please enter a city name.";
+  return;
+}
+
+export function displayError(): void {
+  location.style.marginTop = "2.1rem";
+  error.style.display = "block";
+}
+
+export function removeError(): void {
+  location.style.marginTop = "0rem";
+  error.style.display = "none";
+}
 
 function searchCity(): void {
+  const search = document.getElementById("search") as HTMLElement;
   search.addEventListener("click", function () {
-    const cityName = location.value.trim();
-    if (!cityName) return;
-    const GEOCODING_API = `https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=5&appid=${API_KEY}`;
+    const city: string = location.value.trim();
+    if (!city) changeError();
+    const GEOCODING_API: string = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${Weather.API_KEY}`;
     Weather.getCityData(GEOCODING_API);
+  });
+}
+
+function inputEnterKey(): void {
+  location.addEventListener("keypress", function (event) {
+    if (event.key === "Enter") {
+      const city: string = location.value.trim();
+      if (!city) changeError();
+      const GEOCODING_API: string = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${Weather.API_KEY}`;
+      Weather.getCityData(GEOCODING_API);
+    }
   });
 }
 
 export function loadFunctions(): void {
   searchCity();
+  inputEnterKey();
 }
+
+export * as Interface from "./interface";
--- src/weather.ts
+++ src/weather.ts
@@ -1,18 +1,41 @@
-export * as Weather from "./weather";
+import * as Interface from "./interface";
 
-export async function getCityData(API: string) {
-  const response = await fetch(API, { mode: "cors" });
-  const cityData = await response.json();
-  console.log(cityData);
+export const API_KEY: string = "226dbf12f6c9f3e021556ea66e7c95c9";
+
+export async function getCityData(key: string): Promise<any> {
+  const response: Response = await fetch(key, { mode: "cors" });
+  const data = await response.json();
+  if (!data.length) {
+    Interface.displayError();
+  }
+  const { name, country, lat, lon } = data[0];
+  Interface.removeError();
+  Interface.changeInfo(name, country);
+  getWeatherData(lat, lon, API_KEY);
 }
 
 export async function getWeatherData(
-  latitude: string,
-  longitude: string,
+  latitude: number,
+  longitude: number,
   API: string
-) {
+): Promise<any> {
   const FORECAST_API: string = `https://api.openweathermap.org/data/2.5/forecast/?lat=${latitude}&lon=${longitude}&appid=${API}&units=metric`;
-  const response = await fetch(FORECAST_API, { mode: "cors" });
-  const weatherData = await response.json();
-  console.log(weatherData);
+  const response: Response = await fetch(FORECAST_API, { mode: "cors" });
+  const data = await response.json();
+  filterForecastData(data);
+}
+
+function filterForecastData(data: Data) {
+  const test: any = data.list[0];
+
+  console.log(test.dt_txt);
+}
+export * as Weather from "./weather";
+
+interface Data {
+  list: Array<object>;
+}
+
+interface Main {
+  dt_txt: string;
 }
